5921683eb8c3f0798bb8f77e2176cfebbb64c008
Suyash <singhsuyash012@gmail.com>
2025-08-13 22:23:02 +0530

    Add/Update 1.Easy/findallnumbersdisappeared.cpp solution

--- leetcode-solutions/1.Easy/findallnumbersdisappeared.cpp
+++ leetcode-solutions/1.Easy/findallnumbersdisappeared.cpp
@@ -2,25 +2,31 @@
 LeetCode Problem: 448. Find All Numbers Disappeared in an Array
 Link: https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 Time Complexity: O(n)
- Space Complexity: O(1) 
+ Space Complexity: O(1)
 */
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> findDisappearedNumbers(vector<int>& nums) {
+    vector<int> findDisappearedNumbers(vector<int> &nums)
+    {
         vector<int> result;
 
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size(); i++)
+        {
             int index = abs(nums[i]) - 1;
-            if (nums[index] > 0) {
-                nums[index] = -nums[index]; 
+            if (nums[index] > 0)
+            {
+                nums[index] = -nums[index];
             }
-        }
+                }
 
         // Collect missing numbers
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] > 0) {
-               result.push_back(i + 1); 
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] > 0)
+            {
+                result.push_back(i + 1);
             }
         }
 
