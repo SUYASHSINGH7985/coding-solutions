b88794d6f32b8358cc5456fb38a3a9dd353c9293
Suyash <singhsuyash012@gmail.com>
2025-08-18 05:31:51 +0530

    Solved LC Q326

---
+++ leetcode-solutions/1.Easy/PowerOfThree.cpp
@@ -0,0 +1,19 @@
+// LeetCode Solutions
+// Problem: Power of Three
+// Difficulty: Easy
+// Approach: if n is less than or equal to 0, return false.
+// Otherwise, calculate the logarithm of n base 3 and check if it is an integer.
+// time complexity: O(log n)
+// space complexity: O(1)
+
+class Solution
+{
+public:
+    bool isPowerOfThree(int n)
+    {
+        if (n <= 0)
+            return false;
+        double x = log10(n) / log10(3);
+        return fabs(x - round(x)) < 1e-10;
+    }
+};
