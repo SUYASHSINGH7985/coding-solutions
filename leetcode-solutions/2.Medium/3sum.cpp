50063a6a2b1ff7f521ee5707e877a130d7b3c4c7
Suyash <singhsuyash012@gmail.com>
2025-09-04 01:40:23 +0530

    Solve LC Q.15 using sorting algorithms and two pointer approach

---
+++ leetcode-solutions/2.Medium/3sum.cpp
@@ -0,0 +1,47 @@
+// question link: https://leetcode.com/problems/3sum/
+// approach: sorting and two pointers
+
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end()); // step 1: sort
+
+        int n = nums.size();
+        for (int i = 0; i < n - 2; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue; // skip duplicate i
+
+            int left = i + 1, right = n - 1;
+            while (left < right)
+            {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0)
+                {
+                    result.push_back({nums[i], nums[left], nums[right]});
+
+                    // skip duplicates for left and right
+                    while (left < right && nums[left] == nums[left + 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right - 1])
+                        right--;
+
+                    left++;
+                    right--;
+                }
+                else if (sum < 0)
+                {
+                    left++; // need bigger sum
+                }
+                else
+                {
+                    right--; // need smaller sum
+                }
+            }
+        }
+        return result;
+    }
+};
