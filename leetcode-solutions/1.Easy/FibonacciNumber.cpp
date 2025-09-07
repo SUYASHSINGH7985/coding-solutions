6e28daf6866a9203cab8b9e776b78414ef1acd19
Suyash <singhsuyash012@gmail.com>
2025-09-08 01:05:30 +0530

    Solve LC Q.509 using basic concept of dynamic programming

---
+++ leetcode-solutions/1.Easy/FibonacciNumber.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/fibonacci-number/description/
+// approach:dynamic programming
+class Solution
+{
+public:
+    int fib(int n)
+    {
+
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+
+        int a = 0, b = 1, c;
+        for (int i = 2; i <= n; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+};
