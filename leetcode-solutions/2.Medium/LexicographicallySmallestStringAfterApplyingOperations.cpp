5adb0512558b3cbbab21376479173c18c638d598
Suyash <singhsuyash012@gmail.com>
2025-10-19 09:52:34 +0530

    feat: Solve LC Q.1625

---
+++ leetcode-solutions/2.Medium/LexicographicallySmallestStringAfterApplyingOperations.cpp
@@ -0,0 +1,47 @@
+class Solution
+{
+public:
+    string findLexSmallestString(string s, int a, int b)
+    {
+        queue<string> q;
+        unordered_set<string> visited;
+        string smallest = s;
+
+        q.push(s);
+        visited.insert(s);
+
+        while (!q.empty())
+        {
+            string cur = q.front();
+            q.pop();
+
+            // Update lexicographically smallest
+            if (cur < smallest)
+            {
+                smallest = cur;
+            }
+
+            // Operation 1: Add 'a' to all odd indices
+            string added = cur;
+            for (int i = 1; i < added.size(); i += 2)
+            {
+                added[i] = (added[i] - '0' + a) % 10 + '0';
+            }
+            if (!visited.count(added))
+            {
+                visited.insert(added);
+                q.push(added);
+            }
+
+            // Operation 2: Rotate right by 'b'
+            string rotated = cur.substr(cur.size() - b) + cur.substr(0, cur.size() - b);
+            if (!visited.count(rotated))
+            {
+                visited.insert(rotated);
+                q.push(rotated);
+            }
+        }
+
+        return smallest;
+    }
+};
